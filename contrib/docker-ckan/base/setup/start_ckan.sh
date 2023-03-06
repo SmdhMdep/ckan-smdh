fd1ca53e8293c43feadba59a508484bc47318029
D-Simona-G <120456967+D-Simona-G@users.noreply.github.com>
2023-03-06 16:33:36 +0000

    Update start_ckan.sh

--- contrib/docker-ckan/base/setup/start_ckan.sh
+++ contrib/docker-ckan/base/setup/start_ckan.sh
@@ -2,11 +2,13 @@
 
 # This script is run by the ckan container to start CKAN
 
-# Set up the Secret key used by Beaker and Flask
-# This can be overriden using a CKAN___BEAKER__SESSION__SECRET env var
 echo "Setting up beaker to use the database instead of disk"
 ckan config-tool $CKAN_INI "beaker.session.type=ext:database"
 ckan config-tool $CKAN_INI "beaker.session.url=$CKAN_SQLALCHEMY_URL"
+
+# Update the plugins setting in the ini file with the values defined in the env var
+echo "Loading the following plugins: $CKAN__PLUGINS"
+ckan config-tool $CKAN_INI "ckan.plugins = $CKAN__PLUGINS"
 
 # Run the prerun script to init CKAN and create the default admin user
 sudo -u ckan -EH python3 $APP_DIR/prerun_prod.py
